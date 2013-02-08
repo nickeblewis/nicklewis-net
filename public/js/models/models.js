baaa280bafd18c5e2629e4ad2caec58ed2a9f9b2
Nick Lewis <nick@nicklewis.co.uk>
2013-02-08 17:11:52 +0000

    Update public/js/models/models.js
    Converting the model to RequireJs

--- public/js/models/models.js
+++ public/js/models/models.js
@@ -1,64 +1,70 @@
-// TODO: Implement requireJs using define etc
-window.Article = Backbone.Model.extend({
+define([
+    'underscore',
+    'backbone',
+    ], function(_, Backbone( {
+    var ArticleModel = Backbone.Model.extend({
 
-    urlRoot: "/articles",
-
-    idAttribute: "_id",
-
-    initialize: function () {
-        this.validators = {};
-
-        // TODO: These validations will need to change
-        this.validators.name = function (value) {
-            return value.length > 0 ? {isValid: true} : {isValid: false, message: "You must enter a name"};
-        };
-
-        this.validators.grapes = function (value) {
-            return value.length > 0 ? {isValid: true} : {isValid: false, message: "You must enter a grape variety"};
-        };
-
-        this.validators.country = function (value) {
-            return value.length > 0 ? {isValid: true} : {isValid: false, message: "You must enter a country"};
-        };
-    },
-
-    validateItem: function (key) {
-        return (this.validators[key]) ? this.validators[key](this.get(key)) : {isValid: true};
-    },
-
-    // TODO: Implement Backbone's standard validate() method instead.
-    validateAll: function () {
-
-        var messages = {};
-
-        for (var key in this.validators) {
-            if(this.validators.hasOwnProperty(key)) {
-                var check = this.validators[key](this.get(key));
-                if (check.isValid === false) {
-                    messages[key] = check.message;
+        urlRoot: "/articles",
+    
+        idAttribute: "_id",
+    
+        initialize: function () {
+            this.validators = {};
+    
+            // TODO: These validations will need to change
+            this.validators.name = function (value) {
+                return value.length > 0 ? {isValid: true} : {isValid: false, message: "You must enter a name"};
+            };
+    
+            this.validators.grapes = function (value) {
+                return value.length > 0 ? {isValid: true} : {isValid: false, message: "You must enter a grape variety"};
+            };
+    
+            this.validators.country = function (value) {
+                return value.length > 0 ? {isValid: true} : {isValid: false, message: "You must enter a country"};
+            };
+        },
+    
+        validateItem: function (key) {
+            return (this.validators[key]) ? this.validators[key](this.get(key)) : {isValid: true};
+        },
+    
+        // TODO: Implement Backbone's standard validate() method instead.
+        validateAll: function () {
+    
+            var messages = {};
+    
+            for (var key in this.validators) {
+                if(this.validators.hasOwnProperty(key)) {
+                    var check = this.validators[key](this.get(key));
+                    if (check.isValid === false) {
+                        messages[key] = check.message;
+                    }
                 }
             }
+    
+            return _.size(messages) > 0 ? {isValid: false, messages: messages} : {isValid: true};
+        },
+    
+        defaults: {
+            _id: null,
+            name: "",
+            grapes: "",
+            country: "USA",
+            region: "California",
+            year: "",
+            description: "",
+            picture: null
         }
+    });
 
-        return _.size(messages) > 0 ? {isValid: false, messages: messages} : {isValid: true};
-    },
-
-    defaults: {
-        _id: null,
-        name: "",
-        grapes: "",
-        country: "USA",
-        region: "California",
-        year: "",
-        description: "",
-        picture: null
-    }
-});
-
-window.ArticleCollection = Backbone.Collection.extend({
-
-    model: Article,
-
-    url: "/articles"
-
+    var ArticleCollection = Backbone.Collection.extend({
+    
+        model: Article,
+    
+        url: "/articles"
+    
+    });
+    
+    return ArticleModel;
 });
