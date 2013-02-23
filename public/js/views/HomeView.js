24099826d5f95a5b6a1a252b0063af3f99e52be9
Nick Lewis <nickeblewis@gmail.com>
2013-02-23 16:28:15 +0000

    Added new home model to view

--- public/js/views/HomeView.js
+++ public/js/views/HomeView.js
@@ -2,14 +2,17 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'models/home',
   'text!templates/home/homeTemplate.html'
-], function($, _, Backbone, homeTemplate){
+], function($, _, Backbone, Home, homeTemplate){
 
   var HomeView = Backbone.View.extend({
     el: $("#contentmain"),
 
-    render: function(){      
-      this.$el.html(homeTemplate);
+    render: function(){ 
+      var home = new Home();
+      this.$el.html( _.template(homeTemplate, home.attributes ));
+      return this;
     }
 
   });
