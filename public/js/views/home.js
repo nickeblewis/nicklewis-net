9b46ed2f8f7848adb1616a421901a4b113e57e83
Nick Lewis <nickeblewis@gmail.com>
2013-03-30 17:10:15 +0000

    Added a cool bit of jquery to homepage

--- public/js/views/home.js
+++ public/js/views/home.js
@@ -18,6 +18,29 @@ define([
       var home = new Home();
       $(this.el).html(this.template(home.attributes));
       return this;
+    },
+
+    events: {
+      "click .design" : "designClick",
+      "click .photos" : "photosClick",
+      "click .code" : "codeClick",
+      "click .writing" : "writingClick"
+    },
+
+    designClick: function() {
+      $('#detailbox').text('Design is nice');
+    },
+    photosClick: function() {
+            $('#detailbox').text('Love the photography');
+
+    },
+    codeClick: function() {
+            $('#detailbox').text('Coding cool');
+
+    },
+    writingClick: function() {
+            $('#detailbox').text('Love a bit of writing');
+
     }
   });
 });
