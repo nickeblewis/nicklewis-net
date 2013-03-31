fcf37f54c7967fe3af05f6c9f0b1b3b463fd5f6f
Nick Lewis <nickeblewis@gmail.com>
2013-03-31 18:18:39 +0100

    Reduced items per page to just 10, deleted some redundant code too

--- public/js/views/portfoliolist.js
+++ public/js/views/portfoliolist.js
@@ -5,42 +5,23 @@ define(['jquery', 'underscore', 'backbone', 'isotope', 'utils', 'PortfolioListIt
 
         initialize: function () {
             this.render();
-            
         },
 
         render: function () {
             var portfolio = this.model.models;
             var len = portfolio.length;
-            var startPos = (this.options.page - 1) * 50;
-            var endPos = Math.min(startPos + 50, len);
+            var startPos = (this.options.page - 1) * 10;
+            var endPos = Math.min(startPos + 10, len);
 
-           
             $(this.el).html('<div id="photos"></div>');
 
             for (var i = startPos; i < endPos; i++) {  
 
                 $('#photos', this.el).append(new PortfolioListItemView({model: portfolio[i]}).render().el);
             }
-
-            // $(this.el).append(new Paginator({model: this.model, page: this.options.page}).render().el);
-
-
-            // $(this.el).isotope(
-            //                 {
-            //                     itemSelector: '.item',
-            //                     resizable: false,
-            //                     masonry: { columnWidth: 1 },
-            //                     containerStyle: {
-            //                         position: 'relative',
-            //                         zIndex: 1
-            //                     }
-            //                 }
-            //             );
-
+            
             var _this = this;
-            setTimeout(function() { _this.$el.isotope({ itemSelector: '.element',
-
-            })},0);
+            setTimeout(function() { _this.$el.isotope({ itemSelector: '.element' })},0);
             return this;
         }
     });    
