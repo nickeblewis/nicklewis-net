409b72b68514e5bd60b828a8083ae8bcfbd9a28b
Nick Lewis <nickeblewis@gmail.com>
2013-04-08 23:15:23 +0100

    Fixed home page hover actions

--- public/js/views/home.js
+++ public/js/views/home.js
@@ -31,16 +31,15 @@ define([
       $('#detailbox').text('Design is not just about making a website nice on the eye, it is also about making it tick!');
     },
     photosHover: function() {
-            $('#detailbox').text('Photography is in the heart and soul, good photography helps provide a narrative.');
+      $('#detailbox').text('Photography is in the heart and soul, good photography helps provide a narrative.');
 
     },
     codeHover: function() {
-            $('#detailbox').text('Code, mostly Javascript here is what drives us, not just the sites we build!');
+      $('#detailbox').text('Code, mostly Javascript here is what drives us, not just the sites we build!');
 
     },
     writingHover: function() {
-            $('#detailbox').text('Words are invaluable and as you can tell we love gorgeous typography too!');
-
+      $('#detailbox').text('Words are invaluable and as you can tell we love gorgeous typography too!');
     }
   });
 });
